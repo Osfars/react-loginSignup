4969f1e136d124f87501ccdb1f486d0c711defab
Farouk <farouk.sumana@outlook.com>
2024-08-14 12:22:56 +0000

    k

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,12 +1,50 @@
 import React, { useState } from 'react'
 import './LoginSignup.css'
+import axios from "axios"
 
-import user_icon from '../Assets/person.png'
+import user_icon from '../Assets/person.png';
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
 export const LoginSignup = () => {
     const [action, setAction] = useState("Sign Up");
+    const [username, setUsername] =useState("");
+    const [passwrd, setPasswrd] = useState("");
+
+    const handleUsernameChange = (value) => {
+        setUsername(value);
+      };
+    const handlePasswordChange = (value) => {
+        setPasswrd(value);
+      };
+
+      const handleLogin = () => {
+        const data = {
+          Username: username,
+          Password: passwrd
+        };
+        const url = "https://localhost:44346/aip/test/login";
+    
+        if(data.Username === null || data.Username === '' || data.Password === null || data.Password === '')
+        {
+            alert('fill all the details');
+            return;
+        }
+    
+        axios
+          .post(url, data)
+          .then((result) => {
+            if (result.data === "User is valid") 
+                alert("successfully login");
+            else {
+              alert(result.data);
+            }
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      };
+
   return (
     <div className='container'>
         <div className="header">
@@ -22,12 +60,12 @@ export const LoginSignup = () => {
 
             <div className="input">
                 <img src={email_icon} alt="" />
-                <input type="email" placeholder='email'/>
+                <input type="email" placeholder='email'onChange={(e) => handleUsernameChange(e.target.value)}/>
             </div>
 
             <div className="input">
                 <img src={password_icon} alt="" />
-                <input type="password" placeholder='password'/>
+                <input type="password" placeholder='password' onChange={(e) => handlePasswordChange(e.target.value)}/>
             </div>
             {action === "Login" ? <div></div> : <div className="input">
                 <img src={password_icon} alt="" />
@@ -39,6 +77,13 @@ export const LoginSignup = () => {
         <div className="submit-container">
             <div className={action === "Login" ? "submit gray":"submit"} onClick={() =>{setAction("Sign Up")}}>Sign Up</div>
             <div className={action === "Sign Up" ? "submit gray":"submit"} onClick={() =>{setAction("Login")}}>Login</div>
+        </div>
+        <div className="forgot-password">
+            <div className="farright">
+                <span>
+                    {action === "Login" ? "Sign Up": action === "Sign Up" ? "Login":""}
+                    </span> onClick={() =>{setAction("Login")}}
+             </div>
         </div>
     </div>
   )
